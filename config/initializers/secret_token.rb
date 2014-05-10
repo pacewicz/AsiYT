51edfc1fce4a6fa4e7e13d6c6e1c036774d06475
Patryk Acewicz <patryk.acewicz@gmail.com>
2014-05-10 12:38:14 +0200

    Generated init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsiYT::Application.config.secret_key_base = 'd632cdf689869c8650b5a020fad8d504ca3c068d5cc07fcbbce18597dc3086d140d2ea65508efea5a94778d57a8e76bc668e59f71d68033a80136054b92b6e59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AsiYT::Application.config.session_store :cookie_store, key: '_AsiYT_session'
