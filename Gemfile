1950971762c6ff41d8e7ebb42d59e628b3981c45
skomorowski <szymon.komorowski@hotmail.com>
2014-05-11 20:12:35 +0200

    new static MainView with bootstrap

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+
+gem 'bootstrap-sass', '~> 3.1.1'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 AsiYT::Application.routes.draw do
+  #get "playlists/show"
+
+  root 'playlists#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
