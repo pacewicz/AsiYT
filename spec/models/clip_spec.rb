db4586e51f21ff0bd0f0c77f5123bbb7e50b6632
skomorowski <szymon.komorowski@hotmail.com>
2014-05-22 00:16:47 +0200

    added simple rspec tests for playlist & clip models

--- spec/models/clip_spec.rb
+++ spec/models/clip_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe Clip, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+
+  before :each do
+    @clip = Clip.new
+    @clip.id = 1
+    @clip.playlist_id = 2
+    @clip.yt_id = "o-5-B11Gc2U"
+    @clip.title = "\"Chillstep - CMYK - Yellow Chillstep Mix - by Ideal ...\""
+    @clip.description = nil
+  end
+
+  describe "#new" do
+    it 'returns a new clip object' do
+      @clip.should be_an_instance_of Clip
+    end
+
+    it 'returns nil value of description' do
+      @clip.description.should eql nil
+    end
+
+  end
+
+  describe "#playlist_id" do
+    it "returns the correct playlist_id" do
+      @clip.playlist_id.should eql 2
+    end
+  end
+
+  describe "#title" do
+    it "returns the correct clip title" do
+      @clip.title.should eql "\"Chillstep - CMYK - Yellow Chillstep Mix - by Ideal ...\""
+    end
+  end
+
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Playlist, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+
+  before :each do
+    @playlist = Playlist.new
+    @playlist.id = 1
+    @playlist.user_id = 2
+    @playlist.name = "Playlist name"
+  end
+
+  describe "#new" do
+    it 'returns a new playlist object' do
+      @playlist.should be_an_instance_of Playlist
+    end
+  end
+
+  describe "#playlist" do
+    it "returns the correct user" do
+      @playlist.user_id.should eql 2
+    end
+  end
+
+  describe "#id" do
+    it "returns the correct id" do
+      @playlist.id.should eql 1
+    end
+  end
+
 end
